05b3a7ca042f21d2172aec0a1f366a5dfd5c4f82
roshanparajuli08 <110726965+roshanparajuli08@users.noreply.github.com>
2022-08-06 18:06:15 +0530

    Create Subsets.cpp
    Leetcode solution to problem number 78 Subset

---
+++ Subsets.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+    public:
+    void printSubset(int index,vector<vector<int>>& ans, vector<int>& int_ans , vector<int> &nums){
+        if(index == nums.size()){
+            ans.push_back(int_ans);
+            return;    
+        }  
+        //not take the number
+        printSubset(index+1, ans, int_ans, nums);
+        
+         //take the number
+          int_ans.push_back(nums[index]);
+           printSubset(index+1, ans, int_ans, nums);
+           int_ans.pop_back();     
+    }
+    
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector <int> int_ans;
+        int index = 0;
+        printSubset(index, ans, int_ans, nums);
+        return ans;
+        
+}
+};
